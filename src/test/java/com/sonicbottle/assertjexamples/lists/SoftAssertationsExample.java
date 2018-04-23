890e73c42e2d0848ae325acae6c3a15ed0a0855b
nate-sonicbottle <nate@sonicbottle.com>
2018-04-23 17:06:18 +0100

    Added nonSoftAssertions

--- src/test/java/com/sonicbottle/assertjexamples/lists/SoftAssertationsExample.java
+++ src/test/java/com/sonicbottle/assertjexamples/lists/SoftAssertationsExample.java
@@ -1,16 +1,36 @@
 package com.sonicbottle.assertjexamples.lists;
 
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import org.assertj.core.api.AutoCloseableSoftAssertions;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Test;
 
 public class SoftAssertationsExample {
 
     @Test
-    public void test() {
+    public void nonSoftAssertionExample() {
+        assertTrue("This should fail", 1 == 2);
+        assertTrue("This should work",1 ==1);
+        assertTrue("This should fail", 1 == 3);
+    }
+
+    @Test
+    public void softAssertionsAssertSoftlyExample() {
         SoftAssertions.assertSoftly(softly -> {
             softly.assertThat(1 == 2).as("This should fail").isTrue();
             softly.assertThat(1 == 1).as("This should work").isTrue();
             softly.assertThat(1 == 3).as("This should fail").isTrue();
         });
+    }
+
+    @Test
+    public void autoCloseableSoftAssertionsExample() {
+        try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
+            softly.assertThat(1 == 2).as("This should fail").isTrue();
+            softly.assertThat(1 == 1).as("This should work").isTrue();
+            softly.assertThat(1 == 3).as("This should fail").isTrue();
+        }
     }
 }
